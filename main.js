2361d691aaf9a8472bb11f51cf2263367b4153cf
LeTiolan <knockoffelonmusk@gmail.com>
2026-03-30 21:44:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-import * as THREE from 'three';
-
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 /** =========================================================================
  * ASTRO-BOTANIST: DEEP CORE ENGINE
  * Advanced WebGL Prototype utilizing Custom Physics & Procedural Generation
